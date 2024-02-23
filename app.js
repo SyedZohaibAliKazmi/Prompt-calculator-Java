a52a9a46bd7180a0af5847c696a67ea121f6a466
syed zohaib ali kazmi <syedzohaibali9900@gmail.com>
2024-02-23 20:29:02 +0500

    Prompt Calculator

---
+++ app.js
@@ -0,0 +1,45 @@
+
+// Username 
+
+let userName = prompt("what is your name");
+alert("Welcome " + userName);
+console.log("Username: " + userName)
+
+
+// prompt is use for data colect for people 
+
+let num1 = prompt("enter first number");
+let num2 = prompt("enter second number");
+
+// string ko numbers may convert karkay output deyna 
+
+num1 = Number(num1);
+num2 = Number(num2);
+
+
+// total out put kay answer 
+
+// First for Addition 
+
+let sum = num1 + num2;
+alert("Addition: " + sum)
+console.log("Addition: "+ sum)
+
+// Second for Subtraction 
+
+let minus = num1 - num2;
+alert("Subtraction: " + minus)
+console.log("Subtraction: "+ minus)
+
+// Third for Multiply 
+let multiply = num1 * num2;
+alert("multiplication: " + multiply)
+console.log("multiplication: "+ multiply)
+
+// Fourth for Divide 
+
+let divide =num1 /num2;
+alert("division: "+ divide)
+console.log("division: " + divide)
+
+            // ================End==============
